fc70cdb36b2e16402fbfa4a98cde9ed9d285b45e
emmakoz15 <emmakozlovsky@gmail.com>
2024-03-22 10:44:03 -0400

    created for loop (see comments)

--- script.js
+++ script.js
@@ -11,3 +11,30 @@ function addName() {
 
     nameInput.value = ''// takes the nameInput and deletes it after the user enters it
 }
+
+//displayes the names every time you enter an input - builds brand new list everytime input is entered
+// puts inputs in ul as list items
+function displayNames(){
+    const nameList = document.getElementById("nameList")
+    nameList.innerHTML='' // clears out previous list items
+
+//each time it goes through the loop the entire list is deleted and recreated
+// for loop - given a certain condition  (do something until the end terminator - untill it is less than the amount of input)
+// loop every time it runs goes up by one - rason for of i++  i stands for INCREMENT / INDEX COUNTER OF THE LOOP
+    for (let i = 0; i < namesArray.length; i++) {//i++ adds value of one
+        const name =  namesArray[i]// lets access each mname on array ass you go through loop
+
+        const li = document.createElement('li') // uses js to create html element - li
+        li.className = "list-group-item" // styled name in list
+        
+        const span = document.createElement('span') // uses js to create html element - span
+        span.textContent = name
+
+        li.appendChild(span) // adds a span tag nested inbetween each li tag would look like: <li><span> name</span> </li>
+        nameList.appendChild(li) // adds li tag nested inbetween ul(nameList)
+
+
+    }
+
+}
+document.getElementById('addNameBtn').addEventListener('click', addName)
